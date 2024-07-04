bfd81d78d11277e502a478e63f1fe9a330972b6e
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-07-04 21:44:01 +0530

    update

--- Understanding JSX/day1/src/App.js
+++ Understanding JSX/day1/src/App.js
@@ -5,6 +5,11 @@ let name="World";
 function App() {
   return (
     <>
+      <nav>
+        <li>Home</li>
+        <li>Gallery</li>
+        <li>Contact</li>
+      </nav>
       <div className="container">
         <h1>Hello {name}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur in quibusdam nobis fugiat ullam tempore sequi doloremque impedit soluta, consectetur non, ab assumenda ut reprehenderit odit praesentium natus dolorem aliquam.</p>
