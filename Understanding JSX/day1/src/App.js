93dd5cfa30d974f4434ec06a0414796bd0b2576b
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-07-04 21:43:24 +0530

    update

---
+++ Understanding JSX/day1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name="World";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur in quibusdam nobis fugiat ullam tempore sequi doloremque impedit soluta, consectetur non, ab assumenda ut reprehenderit odit praesentium natus dolorem aliquam.</p>
+      </div>
+    </> //JSX fragment : used to return more than 1 elems
+  );
+}
+
+export default App;
