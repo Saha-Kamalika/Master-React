999832dc5eb4fbb1d9cd0d315db84c978bd9fdb8
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-07-06 15:15:15 +0530

    update

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 function App() {
   return (
     <>
     <Navbar title="Textutils" aboutText="About Textutils"></Navbar>
     <div className="container my-3">
-      <TextForm heading='Enter the text to analyse'/>
+      {/* <TextForm heading='Enter the text to analyse'/> */}
+      <About></About>
     </div>
     </>
   );
