1056c8ff0cc02026a8cd0bc2ae51279ef3627d37
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-07-06 13:01:03 +0530

    update

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,27 +1,60 @@
-import React,{useState} from 'react' // {useState} : hook-> allows to use State and other react features without creating class
+import React, { useState } from 'react';
 
 export default function TextForm(props) {
-    const handleOnChange = (event)=>{
-        //console.log("On change ");
+    const handleOnChange = (event) => {
         setText(event.target.value);
-    }
+    };
+
     const handleUpClick = (event) => {
         event.preventDefault();
         setText(text.toUpperCase());
     };
-    const [text,setText] = useState("Enter text here");
-    // text="new text" //wrong way to change state;
-    // setText("new text") //correct way
+
+    const handleLowClick = (event) => {
+        event.preventDefault();
+        setText(text.toLowerCase());
+    };
+
+    const handleClearClick = (event) => {
+        event.preventDefault();
+        setText('');
+    };
+
+    const [text, setText] = useState("");
+
+    const calculateWordCount = (text) => {
+        return text.trim() === '' ? 0 : text.split("/\s+/").length;
+    };
+
+    const calculateMinuteRead = (wordCount) => {
+        return (wordCount * 0.008).toFixed(2);
+    };
+
+    const wordCount = calculateWordCount(text);
+    const minuteRead = calculateMinuteRead(wordCount);
+
     return (
-        <div>
-            <form>
-            <div className="form-group">
-                <label htmlFor="myBox"><h3>{props.heading}</h3></label>
-                <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange} value={text}></textarea>
+        <>
+            <div className="container">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="myBox"><h3>{props.heading}</h3></label>
+                        <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange} value={text}></textarea>
+                    </div>
+                    <br />
+                    <button className="btn btn-outline-success my-2 my-sm-0 my-3 mx-1" onClick={handleUpClick}>Convert to UPPERCASE</button>
+                    <button className="btn btn-outline-success my-2 my-sm-0 my-3 mx-1" onClick={handleLowClick}>Convert to lowercase</button>
+                    <button className="btn btn-outline-success my-2 my-sm-0 my-3 mx-1" onClick={handleClearClick}>Clear Text</button>
+                </form>
             </div>
-            <br />
-            <button className="btn btn-outline-success my-2 my-sm-0 my-3" onClick={handleUpClick}>Convert to UpperCase</button>
-            </form>
-        </div>
-    )
+            <div className="container my-3">
+                <h2>Your text summary: </h2>
+                <p>{wordCount} words and {text.length} characters</p>
+                <p>{minuteRead} minutes read</p>
+                <h3>Preview: </h3>
+                <p>{text}</p>
+            </div>
+        </>
+    );
 }
+
