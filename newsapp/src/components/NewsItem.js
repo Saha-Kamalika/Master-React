d093c9cd1e780590d04c06dd9dc5c98003ecb15c
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-07-18 21:03:47 +0530

    update

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -7,9 +7,17 @@ export class NewsItem extends Component {
   }
 
   render() {
+    let {title,description,imageUrl}=this.props;
     return (
       <div>
-        This is a news item
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
     )
   }
