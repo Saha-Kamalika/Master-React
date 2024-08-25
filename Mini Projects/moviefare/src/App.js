bbd23b0438fdb59512f1ec7436966de5131a06bd
Saha-Kamalika <kamalikasaha01@gmail.com>
2024-08-25 15:43:11 +0530

    updated

--- Mini Projects/moviefare/src/App.js
+++ Mini Projects/moviefare/src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-// const url='http://www.omdbapi.com?apiKey=6d8b5209';
+const url='http://www.omdbapi.com?apiKey=6d8b5209';
 const App = () => {
-  
+    const searchMovies=async (title)=>{
+        const response=await fetch(`${url}&s=${title}`);
+        const data=await response.json();
+        console.log(data.Search);
+        
+    }
+    useEffect(()=>{
+        searchMovies('Spiderman');
+    },[])
+  return(
+    <h1>Hi</h1>
+  )
 }
 
 export default App;
